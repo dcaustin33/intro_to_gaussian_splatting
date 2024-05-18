99a4661c7f528ae583ee291e3dc6890ffdbf6ac5
dcaustin33 <dcaustin33@gmail.com>
2024-05-18 21:03:58 +0000

    working gaussian code

--- splat/triton_code.py
+++ splat/triton_code.py
@@ -28,27 +28,61 @@ def compute_gaussian_weight(
     
     # each block is loading all the points in view
     points_in_view_offsets = tl.arange(0, num_points * 2)
-    mask = points_in_view_offsets < num_points
     real_points_in_view = tl.load(points_in_view + points_in_view_offsets) > 0
     
+    inverse_covariance_x = tl.load(inverse_covariance + points_in_view_offsets * 4 + 0, mask=real_points_in_view)
+    inverse_covariance_y = tl.load(inverse_covariance + points_in_view_offsets * 4 + 3, mask=real_points_in_view)
+    inverse_covariance_xy = tl.load(inverse_covariance + points_in_view_offsets * 4 + 2, mask=real_points_in_view)
     points_x = tl.load(point_means + points_in_view_offsets * 2, mask=real_points_in_view)
     points_y = tl.load(point_means + points_in_view_offsets * 2 + 1, mask=real_points_in_view)
-    import pdb; pdb.set_trace()
     
-    # pixel_coord = torch.Tensor([pid_0, pid_1]).to("cpu")
-    # difference = point_means - pixel_coord
-    # weight = tl.dot(tl.dot(difference, inverse_covariance), tl.transpose(difference))
-    # weight = tl.exp(-0.5 * weight)
-    # tl.store(point_weight + points_in_view_offsets, weight)
+    difference_x = points_x - 0.0
+    difference_y = points_y - 0.0
+    difference_first = difference_x * inverse_covariance_x + difference_y * inverse_covariance_xy
+    difference_second = difference_x * inverse_covariance_xy + difference_y * inverse_covariance_y
+    final = difference_first * difference_x + difference_second * difference_y
+    tl.store(point_weight + points_in_view_offsets, final, mask=real_points_in_view)
+    
+    
+@triton.jit
+def add_kernel(
+    x_ptr,  # *Pointer* to first input vector.
+    y_ptr,  # *Pointer* to second input vector.
+    output_ptr,  # *Pointer* to output vector.
+    n_elements,  # Size of the vector.
+    BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
+                 # NOTE: `constexpr` so it can be used as a shape value.
+):
+    # There are multiple 'programs' processing different data. We identify which program
+    # we are here:
+    pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
+    # This program will process inputs that are offset from the initial data.
+    # For instance, if you had a vector of length 256 and block_size of 64, the programs
+    # would each access the elements [0:64, 64:128, 128:192, 192:256].
+    # Note that offsets is a list of pointers:
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    # Create a mask to guard memory operations against out-of-bounds accesses.
+    mask = offsets < n_elements
+    # Load x and y from DRAM, masking out any extra elements in case the input is not a
+    # multiple of the block size.
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+    import pdb; pdb.set_trace()
+    output = x + y
+    # Write x + y back to DRAM.
+    tl.store(output_ptr + offsets, output, mask=mask)
 
     
     
 if __name__=="__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
-    num_points = 10
-    point_means = torch.rand_like(torch.zeros(num_points, 2, device=device))
-    points_in_view = torch.randint(0, 2, (num_points, 1), device=device, dtype=torch.int32)
+    num_points = 4
+    point_means = torch.Tensor([[0, 0], [1, 1], [2, 2], [3, 3]]).to(device)
+    points_in_view = torch.Tensor([0, 1, 0, 0]).to(device)
+    # convert to torch.int32
+    points_in_view = points_in_view.to(torch.int32)
     inverse_covariance = torch.rand_like(torch.zeros(num_points, 2, 2, device=device))
     
     point_weight = torch.zeros(num_points, 1, device=device)
@@ -56,5 +90,15 @@ if __name__=="__main__":
     compute_gaussian_weight[1,](point_means, points_in_view, inverse_covariance, point_weight, num_points, 0)
     import pdb; pdb.set_trace()
     
+    # n_elements = 10
+    # x = torch.Tensor([0, 1, 2, 3])
+    # y = torch.Tensor([4, 5, 6, 7])
+    # output = torch.zeros_like(x)
+    # grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
+    # # NOTE:
+    # #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
+    # #  - `triton.jit`'ed functions can be indexed with a launch grid to obtain a callable GPU kernel.
+    # #  - Don't forget to pass meta-parameters as keywords arguments.
+    # add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
     
     
