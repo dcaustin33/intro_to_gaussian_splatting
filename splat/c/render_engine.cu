929869bae92cf1e66fb0d7c2bfc2ac41b231020b
Derek <dcaustin33@gmail.com>
2024-11-29 22:07:23 +0000

    Image memory issue

--- splat/c/render_engine.cu
+++ splat/c/render_engine.cu
@@ -48,6 +48,7 @@ __global__ void render_tile_kernel(
     float* image,
     int* starting_tile_indices,
     int* tile_idx,
+    int* array_indices,
     int image_height,
     int image_width,
     int num_points)
@@ -92,90 +93,95 @@ __global__ void render_tile_kernel(
     int thread_idx = threadIdx.x + threadIdx.y * blockDim.x;
     shared_done_indicator[thread_idx] = false;
     if (starting_tile_indices[correct_tile_idx] == -1) {
-        return;
+        done = true;
     }
 
-    while (true)
-    {
-        num_done = __syncthreads_count(done);
-        if (num_done == thread_dim)
-            break;
+    // while (true)
+    // {
+    //     num_done = __syncthreads_count(done);
+    //     if (num_done == thread_dim)
+    //         break;
 
-        // Calculate global point index for this round
-        point_idx = starting_tile_indices[correct_tile_idx] + round_counter * thread_dim;
+    //     // Calculate global point index for this round
+    //     point_idx = starting_tile_indices[correct_tile_idx] + round_counter * thread_dim;
 
-        // Calculate global memory offset for this point
-        int point_offset = point_idx + thread_idx;
-        if (point_offset >= num_points)
-        {
-            done = true;
-            break;
-        }
-        else
-        {
-            // Load point data into shared memory
-            shared_point_means[thread_idx * 2] = point_means[point_offset * 2];
-            shared_point_means[thread_idx * 2 + 1] = point_means[point_offset * 2 + 1];
-            shared_point_colors[thread_idx * 3] = point_colors[point_offset * 3];
-            shared_point_colors[thread_idx * 3 + 1] = point_colors[point_offset * 3 + 1];
-            shared_point_colors[thread_idx * 3 + 2] = point_colors[point_offset * 3 + 2];
-            shared_point_opacities[thread_idx] = point_opacities[point_offset];
+    //     // Calculate global memory offset for this point
+    //     int point_offset = point_idx + thread_idx;
+    //     int processed_gaussians_idx = array_indices[point_offset];
+    //     printf("processed_gaussians_idx: %d\n Number of points: %d\n", processed_gaussians_idx, num_points);
+    //     if (processed_gaussians_idx >= num_points)
+    //     {
+    //         done = true;
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         // Load point data into shared memory
+    //         // shared_point_means[thread_idx * 2] = point_means[processed_gaussians_idx * 2];
+    //         // shared_point_means[thread_idx * 2 + 1] = point_means[processed_gaussians_idx * 2 + 1];
+    //         // shared_point_colors[thread_idx * 3] = point_colors[processed_gaussians_idx * 3];
+    //         // shared_point_colors[thread_idx * 3 + 1] = point_colors[processed_gaussians_idx * 3 + 1];
+    //         // shared_point_colors[thread_idx * 3 + 2] = point_colors[processed_gaussians_idx * 3 + 2];
+    //         // shared_point_opacities[thread_idx] = point_opacities[processed_gaussians_idx];
 
-            // no need to store the other covariance as its symmetric
-            shared_inverse_covariance_2d[thread_idx * 3] = inverse_covariance_2d[point_offset * 4];
-            shared_inverse_covariance_2d[thread_idx * 3 + 1] = inverse_covariance_2d[point_offset * 4 + 1];
-            shared_inverse_covariance_2d[thread_idx * 3 + 2] = inverse_covariance_2d[point_offset * 4 + 3];
+    //         // // no need to store the other covariance as its symmetric
+    //         // shared_inverse_covariance_2d[thread_idx * 3] = inverse_covariance_2d[processed_gaussians_idx * 4];
+    //         // shared_inverse_covariance_2d[thread_idx * 3 + 1] = inverse_covariance_2d[processed_gaussians_idx * 4 + 1];
+    //         // shared_inverse_covariance_2d[thread_idx * 3 + 2] = inverse_covariance_2d[processed_gaussians_idx * 4 + 3];
 
-            if (tile_idx[point_idx + thread_idx] != correct_tile_idx)
-            {
-                shared_done_indicator[thread_idx] = true;
-            }
-        }
+    //         if (tile_idx[point_idx + thread_idx] != correct_tile_idx)
+    //         {
+    //             shared_done_indicator[thread_idx] = true;
+    //         }
+    //     }
 
-        // wait for all the memory loads to finish
-        __syncthreads();
-        round_counter++;
+    //     // wait for all the memory loads to finish
+    //     __syncthreads();
+    //     round_counter++;
 
-        if (!done)
-        {
-            // render the pixel by iterating through all points until weight or
-            // a done indicator is reached
-            for (int i = 0; i < thread_dim; i++)
-            {
-                if (shared_done_indicator[i])
-                {
-                    done = true;
-                    break;
-                }
-                float weight = compute_pixel_strength(
-                    pixel_x,
-                    pixel_y,
-                    shared_point_means[i * 2],
-                    shared_point_means[i * 2 + 1],
-                    shared_inverse_covariance_2d[i * 3],
-                    shared_inverse_covariance_2d[i * 3 + 1],
-                    shared_inverse_covariance_2d[i * 3 + 2]
-                );
+    //     if (!done)
+    //     {
+    //         // render the pixel by iterating through all points until weight or
+    //         // a done indicator is reached
+    //         for (int i = 0; i < thread_dim; i++)
+    //         {
+    //             if (shared_done_indicator[i])
+    //             {
+    //                 done = true;
+    //                 break;
+    //             }
+    //             float weight = compute_pixel_strength(
+    //                 pixel_x,
+    //                 pixel_y,
+    //                 shared_point_means[i * 2],
+    //                 shared_point_means[i * 2 + 1],
+    //                 shared_inverse_covariance_2d[i * 3],
+    //                 shared_inverse_covariance_2d[i * 3 + 1],
+    //                 shared_inverse_covariance_2d[i * 3 + 2]
+    //             );
 
-                float test_T = total_weight * (1 - sigmoid(shared_point_opacities[i]) * weight);
-                if (test_T < 0.001f)
-                {
-                    done = true;
-                    break;
-                }
-                color.x += test_T * shared_point_colors[i * 3];
-                color.y += test_T * shared_point_colors[i * 3 + 1];
-                color.z += test_T * shared_point_colors[i * 3 + 2];
-                total_weight = test_T;
-            }
-        }
-    }
+    //             float test_T = total_weight * (1 - sigmoid(shared_point_opacities[i]) * weight);
+    //             if (test_T < 0.001f)
+    //             {
+    //                 done = true;
+    //                 break;
+    //             }
+    //             color.x += test_T * shared_point_colors[i * 3];
+    //             color.y += test_T * shared_point_colors[i * 3 + 1];
+    //             color.z += test_T * shared_point_colors[i * 3 + 2];
+    //             total_weight = test_T;
+    //         }
+    //     }
+    // }
     if (pixel_x < image_width && pixel_y < image_height)
     {
-        int pixel_idx = (pixel_y * image_width + pixel_x) * 3;
-        image[pixel_idx] = color.x;
-        image[pixel_idx + 1] = color.y;
-        image[pixel_idx + 2] = color.z;
+        int pixel_idx = (pixel_x * image_height + pixel_y) * 3;
+        if (pixel_idx >= 0 && pixel_idx + 2 < image_width * image_height * 3) 
+        {
+            image[pixel_idx] = color.x;
+            image[pixel_idx + 1] = color.y;
+            image[pixel_idx + 2] = color.z;
+        }
     }
 }
 
@@ -189,6 +195,7 @@ torch::Tensor render_tile_cuda(
     torch::Tensor image,
     torch::Tensor starting_tile_indices,
     torch::Tensor tile_idx,
+    torch::Tensor array_indices,
     int image_height,
     int image_width,
     int num_points)
@@ -200,7 +207,9 @@ torch::Tensor render_tile_cuda(
     int grid_size_x = (image_width + tile_size - 1) / tile_size;
     int grid_size_y = (image_height + tile_size - 1) / tile_size;
     dim3 grid_size(grid_size_x, grid_size_y);
-    
+
+    //print the amount of elements in image tensor
+    printf("Number of elements in image tensor: %zu\n", image.numel());
     render_tile_kernel<<<grid_size, block_size>>>(
         tile_size,
         point_means.data_ptr<float>(),
@@ -210,6 +219,7 @@ torch::Tensor render_tile_cuda(
         image.data_ptr<float>(),
         starting_tile_indices.data_ptr<int>(),
         tile_idx.data_ptr<int>(),
+        array_indices.data_ptr<int>(),
         image_height,
         image_width,
         num_points);
