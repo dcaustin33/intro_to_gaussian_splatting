86e96d620f2927e74d4d5f275341e70047a9afd1
dcaustin33 <dcaustin33@gmail.com>
2025-01-03 05:35:57 -0500

    Color and opacity

---
+++ splat/test/create_image_custom.py
@@ -0,0 +1,41 @@
+import torch
+
+from splat.test.create_image_cpu import Camera, Gaussian, compute_2d_covariance
+from splat.test.partials import gaussianMeanToPixels, pixelCoordToColor
+
+
+def create_image(camera: Camera, gaussian: Gaussian, height: int, width: int):
+    point_ndc = gaussianMeanToPixels.apply(
+        gaussian.mean, 
+        torch.tensor(camera.width), 
+        torch.tensor(camera.height),
+        camera.intrinsic_matrix,
+        camera.extrinsic_matrix,
+    )
+    
+    covariance_2d = compute_2d_covariance(
+        points_homogeneous=gaussian.homogeneous_points,
+        extrinsic_matrix=camera.extrinsic_matrix,
+        covariance_3d=gaussian.covariance,
+        tan_fovY=camera.tan_fovY,
+        tan_fovX=camera.tan_fovX,
+        focal_x=camera.focal_x,
+        focal_y=camera.focal_y,
+    )[0]
+    # point_ndc = torch.tensor(point_ndc, requires_grad=True)
+    inverted_covariance_2d = torch.linalg.inv(covariance_2d)
+    image = torch.zeros((height, width, 3))
+    print("point_ndc", point_ndc)
+    print("gaussian.color", gaussian.color)
+    print("gaussian.opacity", gaussian.opacity)
+    for i in range(height):
+        for j in range(width):
+            image[i, j] = pixelCoordToColor.apply(
+                torch.tensor([[i, j]]),
+                point_ndc,
+                gaussian.color,
+                inverted_covariance_2d,
+                torch.tensor(1.0),
+                gaussian.opacity,
+            )
+    return image, point_ndc
--- splat/test/partials.py
+++ splat/test/partials.py
@@ -6,13 +6,14 @@ from splat.utils import ndc2Pix
 def d_color_wrt_alpha(C: torch.Tensor, T_at_the_time: torch.Tensor) -> torch.Tensor:
     """
     Compute the derivative of color wrt alpha
-    
+
     C should be a nx3 tensor
     T_at_the_time should be a nx1 tensor
-    
+
     Output should be a nx3 tensor
     """
     return C * T_at_the_time
+
 
 def d_alpha_wrt_gaussian_strength(opacity: torch.Tensor) -> torch.Tensor:
     """
@@ -22,24 +23,30 @@ def d_alpha_wrt_gaussian_strength(opacity: torch.Tensor) -> torch.Tensor:
     return torch.sigmoid(opacity)
 
 
-def d_gaussian_strength_wrt_gaussian_weight(gaussian_weight: torch.Tensor) -> torch.Tensor:
+def d_gaussian_strength_wrt_gaussian_weight(
+    gaussian_weight: torch.Tensor,
+) -> torch.Tensor:
     """
     input is a nx1 tensor
     output is a nx1 tensor
     """
     return torch.exp(gaussian_weight).view(-1, 1)
 
-def d_gaussian_weight_wrt_diff(diff: torch.Tensor, inv_covariance: torch.Tensor) -> torch.Tensor:
+
+def d_gaussian_weight_wrt_diff(
+    diff: torch.Tensor, inv_covariance: torch.Tensor
+) -> torch.Tensor:
     """
     Compute the derivative of gaussian weight wrt diff.
     Derivative of x.T @ inv_covariance @ x is (inv_covariance + inv_covariance.T) @ x
-    
+
     diff should be a nx2 tensor
     inv_covariance should be a nx2x2 tensor
-    
+
     output should be a n,2 tensor
     """
     return -0.5 * torch.bmm(inv_covariance + inv_covariance, diff.transpose(1, 2))
+
 
 def d_diff_wrt_mean(n: int) -> torch.Tensor:
     """n, 2, 2 tensor of -1 on diagonal"""
@@ -64,8 +71,8 @@ def d_pixel_space_perspective_transform(
     deriviative = torch.zeros((x_value_before_divide.shape[0], 2, 4))
     deriviative[:, 0, 0] = 1 / w_value_before_divide
     deriviative[:, 1, 1] = 1 / w_value_before_divide
-    deriviative[:, 0, 3] = -x_value_before_divide / w_value_before_divide**2
-    deriviative[:, 1, 3] = -y_value_before_divide / w_value_before_divide**2
+    deriviative[:, 0, 3] = -x_value_before_divide / (w_value_before_divide**2)
+    deriviative[:, 1, 3] = -y_value_before_divide / (w_value_before_divide**2)
     return deriviative
 
 
@@ -83,43 +90,11 @@ def d_world_to_camera(extrinsic_matrix: torch.Tensor) -> torch.Tensor:
     return extrinsic_matrix.T
 
 
-class pixelSpaceToPixels(torch.autograd.Function):
-    @staticmethod
-    def forward(
-        ctx,
-        points_3d: torch.Tensor,
-        width: int,
-        height: int,
-        intrinsic_matrix: torch.Tensor,
-        extrinsic_matrix: torch.Tensor,
-    ) -> torch.Tensor:
-        ctx.save_for_backward(width, height, intrinsic_matrix, extrinsic_matrix, points_3d)
-        points_homogeneous = torch.cat(
-            [points_3d, torch.ones(points_3d.shape[0], 1)], dim=1
-        )
-        points_camera_space = points_homogeneous @ extrinsic_matrix
-        points_pixel_space_before_divide = points_camera_space @ intrinsic_matrix
-        points_pixel_space = points_pixel_space_before_divide[:, :2] / points_pixel_space_before_divide[:, 3].unsqueeze(1)
-        pixel_x = ndc2Pix(points_pixel_space[:, 0], width)
-        pixel_y = ndc2Pix(points_pixel_space[:, 1], height)
-        final_coords = torch.cat([pixel_x.view(-1, 1), pixel_y.view(-1, 1)], dim=1)
-        return final_coords
-
-    @staticmethod
-    def backward(ctx, grad_output: torch.Tensor) -> torch.Tensor:
-        """We are going to kind of do checkpointing and compute the values we need here"""
-        width, height, intrinsic_matrix, extrinsic_matrix, points_3d = ctx.saved_tensors
-        ndc_before_divide = torch.cat([points_3d, torch.ones(points_3d.shape[0], 1)], dim=1) @ extrinsic_matrix @ intrinsic_matrix
-        derivative = d_pixel_space_to_pixels(width, height)
-        derivative = derivative @ d_pixel_space_perspective_transform(ndc_before_divide[:, 0], ndc_before_divide[:, 1], ndc_before_divide[:, 3])
-        derivative = derivative @ d_camera_to_pixel_space(intrinsic_matrix)
-        derivative = derivative @ d_world_to_camera(extrinsic_matrix)
-        gradient = torch.bmm(grad_output.unsqueeze(1), derivative[:, :, :3]).squeeze(1)
-        return gradient, None, None, None, None, None
-    
-    
 def extract_gaussian_weight(
-    pixel: torch.Tensor, mean: torch.Tensor, inv_covariance: torch.Tensor, pdb: bool = False
+    pixel: torch.Tensor,
+    mean: torch.Tensor,
+    inv_covariance: torch.Tensor,
+    pdb: bool = False,
 ) -> torch.Tensor:
     """
     Use the covariance matrix to extract the weight of the point
@@ -133,6 +108,7 @@ def extract_gaussian_weight(
     gaussian_weight = -0.5 * torch.matmul(diff, torch.matmul(inv_covariance, diff.t()))
     actual_weight = torch.exp(gaussian_weight)
     return actual_weight, gaussian_weight
+
 
 def render_pixel(
     x_value: int,
@@ -157,16 +133,64 @@ def render_pixel(
         )
     if test_t < min_weight:
         return
-    return color * current_T * alpha, test_t, current_T, gaussian_strength, exponent_weight
-    
-    
+    return (
+        color * current_T * alpha,
+        test_t,
+        current_T,
+        gaussian_strength,
+        exponent_weight,
+    )
+
+
+class gaussianMeanToPixels(torch.autograd.Function):
+    @staticmethod
+    def forward(
+        ctx,
+        points_3d: torch.Tensor,
+        width: int,
+        height: int,
+        intrinsic_matrix: torch.Tensor,
+        extrinsic_matrix: torch.Tensor,
+    ) -> torch.Tensor:
+        ctx.save_for_backward(
+            width, height, intrinsic_matrix, extrinsic_matrix, points_3d
+        )
+        points_homogeneous = torch.cat(
+            [points_3d, torch.ones(points_3d.shape[0], 1)], dim=1
+        )
+        points_camera_space = points_homogeneous @ extrinsic_matrix
+        points_pixel_space_before_divide = points_camera_space @ intrinsic_matrix
+        points_pixel_space = points_pixel_space_before_divide[
+            :, :2
+        ] / points_pixel_space_before_divide[:, 3].unsqueeze(1)
+        pixel_x = ndc2Pix(points_pixel_space[:, 0], width)
+        pixel_y = ndc2Pix(points_pixel_space[:, 1], height)
+        final_coords = torch.cat([pixel_x.view(-1, 1), pixel_y.view(-1, 1)], dim=1)
+        return final_coords
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor) -> torch.Tensor:
+        """We are going to kind of do checkpointing and compute the values we need here"""
+        width, height, intrinsic_matrix, extrinsic_matrix, points_3d = ctx.saved_tensors
+        ndc_before_divide = (
+            torch.cat([points_3d, torch.ones(points_3d.shape[0], 1)], dim=1)
+            @ extrinsic_matrix
+            @ intrinsic_matrix
+        )
+        derivative = d_pixel_space_to_pixels(width, height)
+        derivative = derivative @ d_pixel_space_perspective_transform(ndc_before_divide[:, 0], ndc_before_divide[:, 1], ndc_before_divide[:, 3])
+        derivative = derivative @ d_camera_to_pixel_space(intrinsic_matrix)
+        derivative = derivative @ d_world_to_camera(extrinsic_matrix)
+        gradient = torch.bmm(grad_output.unsqueeze(1), derivative).squeeze(1)
+        return gradient[:, :3], None, None, None, None, None
+
 class pixelCoordToColor(torch.autograd.Function):
     @staticmethod
     def forward(
-        ctx, 
-        pixel_coords: torch.Tensor, 
-        gaussian_coords: torch.Tensor, 
-        color: torch.Tensor, 
+        ctx,
+        pixel_coords: torch.Tensor,
+        gaussian_coords: torch.Tensor,
+        color: torch.Tensor,
         inv_covariance_2d: torch.Tensor,
         current_T: torch.Tensor,
         opacity: torch.Tensor,
@@ -176,25 +200,39 @@ class pixelCoordToColor(torch.autograd.Function):
         gaussian_coords: nx2 tensor
         color: nx3 tensor
         inv_covariance_2d: nx2x2 tensor
-        current_T: nx1 tensor   
+        current_T: nx1 tensor
         opacity: nx1 tensor
         """
         diff = pixel_coords - gaussian_coords
         diff = diff.unsqueeze(-1).transpose(1, 2)
-        gaussian_weight = -0.5 * torch.matmul(diff, torch.matmul(inv_covariance_2d, diff.transpose(1, 2)))
+        gaussian_weight = -0.5 * torch.matmul(
+            diff, torch.matmul(inv_covariance_2d, diff.transpose(1, 2))
+        )
         actual_weight = torch.exp(gaussian_weight).view(-1, 1)
         alpha = actual_weight * torch.sigmoid(opacity)
-        ctx.save_for_backward(color, inv_covariance_2d, current_T, opacity, gaussian_weight, diff)
+        ctx.save_for_backward(
+            color, inv_covariance_2d, current_T, opacity, gaussian_weight, diff, alpha, actual_weight
+        )
         return color * current_T * alpha
 
-    
     @staticmethod
     def backward(ctx, grad_output: torch.Tensor) -> torch.Tensor:
-        color, inv_covariance_2d, current_T, opacity, gaussian_weight, diff = ctx.saved_tensors
+        color, inv_covariance_2d, current_T, opacity, gaussian_weight, diff, alpha, actual_weight = (
+            ctx.saved_tensors
+        )
         derivative = d_color_wrt_alpha(color, current_T).view(-1, 3, 1)
         derivative = derivative * d_alpha_wrt_gaussian_strength(opacity).unsqueeze(1)
-        derivative = derivative * d_gaussian_strength_wrt_gaussian_weight(gaussian_weight).unsqueeze(1)
-        derivative = torch.bmm(derivative, d_gaussian_weight_wrt_diff(diff, inv_covariance_2d).transpose(1, 2))
+        derivative = derivative * d_gaussian_strength_wrt_gaussian_weight(
+            gaussian_weight
+        ).unsqueeze(1)
+        derivative = torch.bmm(
+            derivative,
+            d_gaussian_weight_wrt_diff(diff, inv_covariance_2d).transpose(1, 2),
+        )
         derivative = torch.bmm(derivative, d_diff_wrt_mean(diff.shape[0]))
-        deriviative = torch.bmm(grad_output.unsqueeze(1), derivative).squeeze(1)
-        return None, deriviative, None, None, None, None, None
+        gradient = torch.bmm(grad_output.unsqueeze(1), derivative).squeeze(1)
+        color_gradient = grad_output * (current_T * alpha)
+        # import pdb; pdb.set_trace()
+        opacity_gradient = (grad_output * (color * current_T)).sum(dim=1, keepdim=True)
+        opacity_gradient *= actual_weight * (torch.sigmoid(opacity) * (1 - torch.sigmoid(opacity)))
+        return None, gradient, color_gradient, None, None, opacity_gradient.sum(dim=0)
