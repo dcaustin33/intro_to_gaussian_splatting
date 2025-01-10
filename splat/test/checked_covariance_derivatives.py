ba0cc1eb314921ee89d42ae6873508edbd04435c
dcaustin33 <dcaustin33@gmail.com>
2025-01-10 15:14:00 -0500

    Checked all the covariance derivatives

---
+++ splat/test/checked_covariance_derivatives.py
@@ -0,0 +1,321 @@
+"""
+This is updated from the test_grads.ipynb as opposed to the covariance_derivatives.py 
+file which is unchecked. I need to check all of these
+"""
+
+import torch
+
+from splat.utils import build_rotation
+
+
+def d_inv_wrt_a(
+    a: torch.Tensor,
+    b: torch.Tensor,
+    c: torch.Tensor,
+    d: torch.Tensor,
+    grad_output: torch.Tensor,
+):
+    """
+    All tensors are nx1 tensors - returns a nx1 tensor by summing over the last dimension
+    """
+    det = a * d - b * c
+    deriv = -1 * (d**2) / (det**2) * grad_output[:, 0, 0]
+    deriv += (b * d) / (det**2) * grad_output[:, 0, 1]
+    deriv += (c * d) / (det**2) * grad_output[:, 1, 0]
+    deriv += -1 * (b * c) / (det**2) * grad_output[:, 1, 1]
+    return deriv
+
+
+def d_inv_wrt_b(
+    a: torch.Tensor,
+    b: torch.Tensor,
+    c: torch.Tensor,
+    d: torch.Tensor,
+    grad_output: torch.Tensor,
+):
+    det = a * d - b * c
+    deriv = (c * d) / (det**2) * grad_output[:, 0, 0]
+    deriv += -1 * (a * d) / (det**2) * grad_output[:, 0, 1]
+    deriv += -1 * (c * c) / (det**2) * grad_output[:, 1, 0]
+    deriv += (a * c) / (det**2) * grad_output[:, 1, 1]
+    return deriv
+
+
+def d_inv_wrt_c(
+    a: torch.Tensor,
+    b: torch.Tensor,
+    c: torch.Tensor,
+    d: torch.Tensor,
+    grad_output: torch.Tensor,
+):
+    det = a * d - b * c
+    deriv = (b * d) / (det**2) * grad_output[:, 0, 0]
+    deriv += -1 * (b * b) / (det**2) * grad_output[:, 0, 1]
+    deriv += -1 * (a * d) / (det**2) * grad_output[:, 1, 0]
+    deriv += (a * b) / (det**2) * grad_output[:, 1, 1]
+    return deriv
+
+
+def d_inv_wrt_d(
+    a: torch.Tensor,
+    b: torch.Tensor,
+    c: torch.Tensor,
+    d: torch.Tensor,
+    grad_output: torch.Tensor,
+):
+    det = a * d - b * c
+    deriv = -1 * (b * c) / (det**2) * grad_output[:, 0, 0]
+    deriv += (a * b) / (det**2) * grad_output[:, 0, 1]
+    deriv += (a * c) / (det**2) * grad_output[:, 1, 0]
+    deriv += -1 * (a * a) / (det**2) * grad_output[:, 1, 1]
+    return deriv
+
+
+class invert_2x2_matrix(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, matrix: torch.Tensor):
+        """input is a nx2x2 tensor, returns the inverse of each 2x2 matrix"""
+        ctx.save_for_backward(matrix)
+        det = matrix[:, 0, 0] * matrix[:, 1, 1] - matrix[:, 0, 1] * matrix[:, 1, 0]
+        # Create empty nx2x2 tensor for the inverted matrices
+        final_matrices = torch.zeros_like(matrix)
+
+        # Fill in the inverted matrix elements using the 2x2 matrix inverse formula
+        final_matrices[:, 0, 0] = matrix[:, 1, 1] / det
+        final_matrices[:, 0, 1] = -matrix[:, 0, 1] / det
+        final_matrices[:, 1, 0] = -matrix[:, 1, 0] / det
+        final_matrices[:, 1, 1] = matrix[:, 0, 0] / det
+
+        if torch.isinf(final_matrices).any():
+            raise RuntimeError("Infinite values in final matrices")
+        return final_matrices
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        """grad_output is a nx2x2 tensor, returns the gradient of the inverse of each 2x2 matrix"""
+        matrix = ctx.saved_tensors[0]
+        a = matrix[:, 0, 0]
+        b = matrix[:, 0, 1]
+        c = matrix[:, 1, 0]
+        d = matrix[:, 1, 1]
+        grad_a = d_inv_wrt_a(a, b, c, d, grad_output)
+        grad_b = d_inv_wrt_b(a, b, c, d, grad_output)
+        grad_c = d_inv_wrt_c(a, b, c, d, grad_output)
+        grad_d = d_inv_wrt_d(a, b, c, d, grad_output)
+        return torch.stack([grad_a, grad_b, grad_c, grad_d], dim=-1).view(matrix.shape)
+
+
+class covariance_3d_to_covariance_2d(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, covariance_3d: torch.Tensor, M: torch.Tensor):
+        """
+        Covariance 3d is the nx3x3 covariance matrix.
+        M is the J@W.T matrix. this is a 3x3 matrix
+        To get the covariance 2d we do M.T @ covariance_3d @ M
+        """
+        first_mult = M.transpose(0, 1) @ covariance_3d
+        second_mult = first_mult @ M
+        ctx.save_for_backward(M)
+        return second_mult
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        (M,) = ctx.saved_tensors
+
+        # Derivative of (M^T * C * M) w.r.t. C = M * grad_output * M^T
+        grad_cov3d = M @ grad_output @ M.transpose(0, 1)
+        return grad_cov3d, None
+
+
+class R_S_To_M(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, R: torch.Tensor, S: torch.Tensor):
+        """R is a nx3x3 rotation matrix, S is a nx3x3 scale matrix"""
+        ctx.save_for_backward(R, S)
+        return torch.einsum("nij,njk->nik", R, S)
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        R, S = ctx.saved_tensors
+        grad_R = torch.einsum("nij,njk->nik", grad_output, S.transpose(1, 2))
+        grad_S = torch.einsum("nij,njk->nik", R.transpose(1, 2), grad_output)
+        return grad_R, grad_S
+
+
+class M_To_Covariance(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, M: torch.Tensor):
+        ctx.save_for_backward(M)
+        return M.pow(2)
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        M = ctx.saved_tensors[0]
+        return 2 * grad_output * M
+
+
+def d_r_wrt_qr(quats: torch.Tensor, n: int) -> torch.Tensor:
+    """
+    Compute the derivative of m wrt quats
+    quats is nx4 tensor
+    shape is nx3 tensor
+    """
+    qr = quats[:, 0]
+    qi = quats[:, 1]
+    qj = quats[:, 2]
+    qk = quats[:, 3]
+
+    derivative = torch.zeros((n, 3, 3))
+    derivative[:, 0, 1] = -qk
+    derivative[:, 0, 2] = qj
+    derivative[:, 1, 0] = qk
+    derivative[:, 1, 2] = -qi
+    derivative[:, 2, 0] = -qj
+    derivative[:, 2, 1] = qi
+
+    return 2 * derivative
+
+
+def d_r_wrt_qi(quats: torch.Tensor, n: int) -> torch.Tensor:
+    """
+    Compute the derivative of m wrt quats
+    quats is nx4 tensor
+    shape is nx3 tensor
+    """
+    qr = quats[:, 0]
+    qi = quats[:, 1]
+    qj = quats[:, 2]
+    qk = quats[:, 3]
+
+    derivative = torch.zeros((n, 3, 3))
+    derivative[:, 0, 1] = qj
+    derivative[:, 0, 2] = qk
+    derivative[:, 1, 0] = qj
+    derivative[:, 1, 1] = -2 * qi
+    derivative[:, 1, 2] = -qr
+    derivative[:, 2, 0] = qk
+    derivative[:, 2, 1] = qr
+    derivative[:, 2, 2] = -2 * qi
+    return 2 * derivative
+
+
+def d_r_wrt_qj(quats: torch.Tensor, n: int) -> torch.Tensor:
+    """
+    Compute the derivative of m wrt quats
+    quats is nx4 tensor
+    shape is nx3 tensor
+    """
+    qr = quats[:, 0]
+    qi = quats[:, 1]
+    qj = quats[:, 2]
+    qk = quats[:, 3]
+
+    derivative = torch.zeros((n, 3, 3))
+    derivative[:, 0, 0] = -2 * qj
+    derivative[:, 0, 1] = qi
+    derivative[:, 0, 2] = qr
+    derivative[:, 1, 0] = qi
+    derivative[:, 1, 1] = 0
+    derivative[:, 1, 2] = qk
+    derivative[:, 2, 0] = -qr
+    derivative[:, 2, 1] = qk
+    derivative[:, 2, 2] = -2 * qj
+    return 2 * derivative
+
+
+def d_r_wrt_qk(quats: torch.Tensor, n: int) -> torch.Tensor:
+    """
+    Compute the derivative of m wrt quats
+    quats is nx4 tensor
+    shape is nx3 tensor
+    """
+    qr = quats[:, 0]
+    qi = quats[:, 1]
+    qj = quats[:, 2]
+    qk = quats[:, 3]
+
+    derivative = torch.zeros((n, 3, 3))
+    derivative[:, 0, 0] = -2 * qk
+    derivative[:, 0, 1] = -qr
+    derivative[:, 0, 2] = qi
+    derivative[:, 1, 0] = qr
+    derivative[:, 1, 1] = -2 * qk
+    derivative[:, 1, 2] = qj
+    derivative[:, 2, 0] = qi
+    derivative[:, 2, 1] = qj
+    derivative[:, 2, 2] = 0
+    return 2 * derivative
+
+
+class quats_to_R(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, quats: torch.Tensor) -> torch.Tensor:
+        ctx.save_for_backward(quats)
+        R = build_rotation(quats, normalize=False)
+        return R
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor) -> torch.Tensor:
+        """
+        grad output is nx3x3 and the jacobian nx3x3x4 so deriv_wrt_qr
+        is nx3x3 where each entry is the derivative wrt one element in the final wrt r
+        """
+        quats = ctx.saved_tensors[0]
+        deriv_wrt_qr = d_r_wrt_qr(quats, quats.shape[0])
+        deriv_wrt_qi = d_r_wrt_qi(quats, quats.shape[0])
+        deriv_wrt_qj = d_r_wrt_qj(quats, quats.shape[0])
+        deriv_wrt_qk = d_r_wrt_qk(quats, quats.shape[0])
+
+        deriv_wrt_qr = (
+            (grad_output * deriv_wrt_qr).sum(dim=(1, 2), keepdim=True).squeeze(2)
+        )
+        deriv_wrt_qi = (
+            (grad_output * deriv_wrt_qi).sum(dim=(1, 2), keepdim=True).squeeze(2)
+        )
+        deriv_wrt_qj = (
+            (grad_output * deriv_wrt_qj).sum(dim=(1, 2), keepdim=True).squeeze(2)
+        )
+        deriv_wrt_qk = (
+            (grad_output * deriv_wrt_qk).sum(dim=(1, 2), keepdim=True).squeeze(2)
+        )
+        return torch.cat(
+            [deriv_wrt_qr, deriv_wrt_qi, deriv_wrt_qj, deriv_wrt_qk], dim=1
+        )
+
+
+class normalize_quats(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, quats: torch.Tensor):
+        """Quats are a nx4 tensor"""
+        ctx.save_for_backward(quats)
+        return quats / quats.norm(dim=1, keepdim=True)
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        quats = ctx.saved_tensors[0]
+        norm = quats.norm(dim=1, keepdim=True)
+        norm_cube = norm**3
+
+        quats_outer = torch.einsum("ni,nj->nij", quats, quats)
+        eye = torch.eye(4, dtype=quats.dtype, device=quats.device).unsqueeze(0)
+
+        jacobian = (eye / norm.unsqueeze(2)) - (quats_outer / norm_cube.unsqueeze(2))
+        grad_input = torch.einsum("nij,nj->ni", jacobian, grad_output)
+        return grad_input
+
+
+class scale_to_s_matrix(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, s: torch.Tensor):
+        """Takes the nx3 tensor and returns the nx3x3 diagonal matrix"""
+        ctx.save_for_backward(s)
+        return torch.diag_embed(s)
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        """Grad output is a nx3x3 tensor"""
+        s = ctx.saved_tensors[0]
+        deriv_wr_s1 = grad_output[:, 0, 0].view(-1, 1)
+        deriv_wr_s2 = grad_output[:, 1, 1].view(-1, 1)
+        deriv_wr_s3 = grad_output[:, 2, 2].view(-1, 1)
+        return torch.cat([deriv_wr_s1, deriv_wr_s2, deriv_wr_s3], dim=1)
---
+++ splat/test/gaussian_weight_derivatives.py
@@ -0,0 +1,77 @@
+import torch
+from torch.autograd.gradcheck import gradcheck
+
+from splat.utils import build_rotation
+
+
+class final_color(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, color: torch.Tensor, current_T: torch.Tensor, alpha: torch.Tensor):
+        """Color is a nx3 tensor, weight is a nx1 tensor, alpha is a nx1 tensor"""
+        ctx.save_for_backward(color, current_T, alpha)
+        return color * current_T * alpha
+    
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        """Output of forward is a nx3 tensor so the grad_output is a nx3 tensor"""
+        color, current_T, alpha = ctx.saved_tensors
+        grad_color = grad_output * current_T * alpha
+        grad_alpha = (grad_output * color * current_T).sum(dim=1, keepdim=True)
+        return grad_color, None, grad_alpha
+    
+class get_alpha(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, gaussian_strength: torch.Tensor, unactivated_opacity: torch.Tensor):
+        """Gaussian strength is a nx1 tensor, unactivated opacity is a nx1 tensor"""
+        ctx.save_for_backward(gaussian_strength, unactivated_opacity)
+        activated_opacity = torch.sigmoid(unactivated_opacity)
+        return gaussian_strength * activated_opacity
+    
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        """Output of forward is a nx1 tensor so the grad_output is a nx1 tensor"""
+        gaussian_strength, unactivated_opacity = ctx.saved_tensors
+        derivative_sigmoid = torch.sigmoid(unactivated_opacity) * (1 - torch.sigmoid(unactivated_opacity))
+        grad_gaussian_strength = grad_output * torch.sigmoid(unactivated_opacity)
+        grad_unactivated_opacity = grad_output * gaussian_strength * derivative_sigmoid
+        return grad_gaussian_strength, grad_unactivated_opacity
+
+
+class gaussian_exp(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, gaussian_weight: torch.Tensor):
+        ctx.save_for_backward(gaussian_weight)
+        return torch.exp(gaussian_weight)
+    
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        gaussian_weight = ctx.saved_tensors[0]
+        grad_gaussian_weight = grad_output * torch.exp(gaussian_weight)
+        return grad_gaussian_weight
+    
+class gaussian_weight(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, gaussian_mean: torch.Tensor, inverted_covariance: torch.Tensor, pixel: torch.Tensor):
+        """
+        Pixel means are a nx2 tensor, inverted covariance is a 2x2 tensor, pixel is a nx2 tensor
+        Outputs a nx1 tensor
+        """
+        ctx.save_for_backward(gaussian_mean, inverted_covariance, pixel)
+        diff = (pixel - gaussian_mean).unsqueeze(1)
+        # 2x2 * 2x1 = 2x1
+        inv_cov_mult = torch.einsum('bij,bjk->bik', inverted_covariance, diff.transpose(1, 2))
+        return -0.5 * torch.einsum('bij,bjk->bik', diff, inv_cov_mult).squeeze(-1)
+    
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        """Output of forward is a nx1 tensor so the grad_output is a nx1 tensor"""
+        gaussian_mean, inverted_covariance, pixel = ctx.saved_tensors
+        diff = (pixel - gaussian_mean).unsqueeze(1)  # nx2x1
+
+        deriv_wrt_inv_cov = -0.5 * torch.einsum("bij,bjk->bik", diff.transpose(1, 2), diff)
+        grad_inv_cov = grad_output * deriv_wrt_inv_cov  # output is nx2x2
+
+        deriv_wrt_diff = -0.5 * 2 * torch.einsum("bij,bjk->bik", diff, inverted_covariance)
+        deriv_wrt_gaussian_mean = -1
+        grad_gaussian_mean = torch.einsum("bi,bij->bj", grad_output, deriv_wrt_diff) * deriv_wrt_gaussian_mean
+        return grad_gaussian_mean, grad_inv_cov, None
